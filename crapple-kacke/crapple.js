e45cbedbb0a93ee1bdb40a54bf6987459da637f4
Alexander01998 <contact.wurstimperium@gmail.com>
2015-05-08 21:19:56 +0200

    Amimation part 2
    Alles fertig. :tada:

--- crapple-kacke/crapple.js
+++ crapple-kacke/crapple.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   window.setTimeout(function(){
-    $('#kacke').removeClass("above-window");
+    $('#kacke').removeClass("kacke-0");
   }, 2000);
+  window.setTimeout(function(){
+    $('#kacke').addClass("kacke-1");
+    $('#crapple').removeClass("crapple-0");
+    $('#crapple').addClass("crapple-1");
+  }, 3000);
+  window.setTimeout(function(){
+    $('#kacke').addClass("kacke-2");
+  }, 4000);
 })
