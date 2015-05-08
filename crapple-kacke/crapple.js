c376280b8439e84343c0a55fa6a419b34b66c1c0
Alexander01998 <contact.wurstimperium@gmail.com>
2015-05-08 21:36:54 +0200

    Bessere Dreh-Animation

--- crapple-kacke/crapple.js
+++ crapple-kacke/crapple.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
     $('#kacke').removeClass("kacke-0");
   }, 2000);
   window.setTimeout(function(){
-    $('#kacke').addClass("kacke-1");
     $('#crapple').removeClass("crapple-0");
     $('#crapple').addClass("crapple-1");
   }, 3000);
   window.setTimeout(function(){
-    $('#kacke').addClass("kacke-2");
+    $('#kacke').addClass("kacke-0");
   }, 4000);
 })
