acfd1d4911bfded2e76279387538c3e69ad2b690
Alexander01998 <contact.wurstimperium@gmail.com>
2015-05-15 18:11:07 +0200

    Positionen von Olli & Arm

--- olli-hat-hunger/olli.js
+++ olli-hat-hunger/olli.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
   window.setTimeout(function(){
-    $('#kacke').removeClass("kacke-0");
+    $('#arm').addClass("move");
   }, 2000);
-  window.setTimeout(function(){
-    $('#crapple').removeClass("crapple-0");
-    $('#crapple').addClass("crapple-1");
-  }, 3000);
-  window.setTimeout(function(){
-    $('#kacke').addClass("kacke-0");
-  }, 4000);
 })
