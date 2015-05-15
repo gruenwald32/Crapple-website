055fbcd2898e7318466a49eb253969b270882aad
Alexander01998 <contact.wurstimperium@gmail.com>
2015-05-15 19:13:30 +0200

    Rest der Animation

--- olli-hat-hunger/olli.js
+++ olli-hat-hunger/olli.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
   window.setTimeout(function(){
     $('#arm').addClass("move");
   }, 2000);
+  window.setTimeout(function(){
+    $('#crapple').removeClass("crapple-0");
+    $('#crapple').addClass("crapple-1");
+  }, 3500);
+  window.setTimeout(function(){
+    $('#arm').removeClass("move");
+  }, 4000);
 })
